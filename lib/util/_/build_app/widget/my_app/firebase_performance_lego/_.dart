803c3e04bb7990115cfdbd47c7f45b439e6d6e67
june lee <melodysdreamj@gmail.com>
2024-07-24 16:42:14 +0900

    Initial commit

---
+++ lib/util/_/build_app/widget/my_app/firebase_performance_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverFirebasePerformanceLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
---
+++ lib/util/_/build_app/widget/run_app/firebase_performance_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverRunApp()
+// Widget coverFirebasePerformanceLego(Widget widget) {
+//   return widget;
+// }
